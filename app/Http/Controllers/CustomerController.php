71f0f3e71e7ebb592d551afedd1c1ea4b345eb50
Emmanuel <emmanuel.nwokedi@gmail.com>
2019-10-13 11:04:10 +0100

    API route and customer controller amended to be able to serve APIs.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -14,6 +14,29 @@ class CustomerController extends Controller
         return view('customer.index', compact('customers'));
     }
 
+    public function getCustomers()
+    {
+        if ($customers = Customer::all()->toJson(JSON_PRETTY_PRINT)) {
+            return response($customers, 200);
+        } else {
+            return response()->json([
+                "message" => "Stundents not found"
+            ], 404);
+        }
+    }
+
+    public function getCustomer($id)
+    {
+        if (Customer::where('id', $id)->exists()) {
+            $customer = Customer::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($customer, 200);
+        } else {
+            return response()->json([
+                "message" => "Customer not found"
+            ], 404);
+        }
+    }
+
     public function create()
     {
         $customer = new Customer();
@@ -31,7 +54,7 @@ class CustomerController extends Controller
 
     public function show(Customer $customer)
     {
-        
+
 
         return view('customer.show', compact('customer'));
     }
